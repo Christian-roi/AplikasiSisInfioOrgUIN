65050d2ea7c172bd1e1dece4a582b580682cec3e
christian roi <{my>
2023-08-18 09:24:34 +0700

    Add Layout Forms

---
+++ app/src/main/java/com/example/appuinsu/AnggotaAdminForm.java
@@ -0,0 +1,31 @@
+package com.example.appuinsu;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AnggotaAdminForm extends AppCompatActivity {
+
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anggota_admin_form);
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    public void onBackPressed() {
+        Intent back = new Intent(this, AnggotaPage.class);
+        startActivity(back);
+        finish();
+    }
+}
